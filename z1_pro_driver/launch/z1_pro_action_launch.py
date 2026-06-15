763047b46fc926cd5226b5eab7e29e0cdd58fab4
Ozer Ozkahraman <ozero@kth.se>
2026-06-15 17:26:41 +0200

    z1 action rosparams nicer in launch

--- z1_pro_driver/launch/z1_pro_action_launch.py
+++ z1_pro_driver/launch/z1_pro_action_launch.py
@@ -3,6 +3,15 @@ from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 
+try:
+    from smarc_msgs.msg import Topics as SmarcTopics
+except ImportError:
+    # If smarc_msgs is not available, define a placeholder Topics class.
+    class SmarcTopics:
+        ODOM_TOPIC = "odom"
+
+from z1_pro_msgs.msg import Topics as Z1ProTopics
+
 
 def generate_launch_description():
     # Namespace as command line argument.
@@ -14,6 +23,14 @@ def generate_launch_description():
         "use_sim_time", default_value='False', description="Use simulation time.")
     use_sim_time = LaunchConfiguration("use_sim_time")
 
+    odom_topic_arg = DeclareLaunchArgument(
+        "odom_topic", default_value=SmarcTopics.ODOM_TOPIC, description="Topic for odometry data.")
+    odom_topic = LaunchConfiguration("odom_topic")
+
+    img_poi_topic_arg = DeclareLaunchArgument(
+        "img_poi_topic", default_value=Z1ProTopics.IMAGE_POI_TOPIC, description="Topic for image POI data.")
+    img_poi_topic = LaunchConfiguration("img_poi_topic")
+
 
     # And finally, launch the action server for gimbal control.
     gimbal_action_server_node = Node(
@@ -23,12 +40,16 @@ def generate_launch_description():
         namespace=robot_name,
         output="screen",
         parameters=[{
-            "use_sim_time": use_sim_time
+            "use_sim_time": use_sim_time,
+            "odom_topic": odom_topic,
+            "img_poi_topic": img_poi_topic
         }]
     )
 
     return LaunchDescription([
         robot_name_arg,
         sim_time_arg,
+        odom_topic_arg,
+        img_poi_topic_arg,
         gimbal_action_server_node 
     ])
